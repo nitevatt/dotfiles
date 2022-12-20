1a2582b525bd2e2cb1f628c3690de0a42093b2d4
nitevatt <nitevatt@outlook.com>
2022-12-20 14:52:25 +0100

    feat(telescope): add project extension

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -2,6 +2,21 @@ local M = {}
 
 M.setup = function(use)
   use { "nvim-telescope/telescope.nvim", tag = "0.1.0", requires = "nvim-lua/plenary.nvim" }
+  use "nvim-telescope/telescope-project.nvim"
+end
+
+local function load_extension(name)
+  local ok, telescope = pcall(require, "telescope")
+
+  if not ok then
+    return
+  end
+
+  pcall(telescope.load_extension, name)
+
+  if telescope.extensions.project then
+    vim.keymap.set("n", "<leader>r", telescope.extensions.project.project, {})
+  end
 end
 
 M.config = function()
@@ -10,6 +25,8 @@ M.config = function()
   if not ok then
     return
   end
+
+  load_extension('project')
 
   vim.keymap.set("n", "<leader>fe", function() builtin.buffers { sort_lastused = true, sort_mru = true } end, {})
   vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
