e90c27484a68b9084b26472429779480c0764cd4
nitevatt <nitevatt@outlook.com>
2022-12-22 16:14:15 +0100

    feat: change mappings for telescope

--- nvim/.config/nvim/lua/config/plugins/telescope.lua
+++ nvim/.config/nvim/lua/config/plugins/telescope.lua
@@ -10,8 +10,9 @@ M.config = function()
 
   telescope.load_extension("project")
 
-  vim.keymap.set("n", "<leader>fe", function() builtin.buffers { sort_lastused = true, sort_mru = true } end, {})
-  vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+  vim.keymap.set("n", "<leader>fe", function() builtin.buffers({ sort_lastused = true, sort_mru = true }) end, {})
+  vim.keymap.set("n", "<leader>ff", builtin.git_files, {})
+  vim.keymap.set("n", "<leader>fa", builtin.find_files({ hidden = true }), {})
   vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols, {})
   vim.keymap.set("n", "<leader>fw", builtin.live_grep, {})
 
