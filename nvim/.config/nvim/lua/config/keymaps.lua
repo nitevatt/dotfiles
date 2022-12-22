b0ed7bfbeb2e6c0c32117cd51f3511796b1a1d86
nitevatt <nitevatt@outlook.com>
2022-12-22 12:02:30 +0100

    feat: add keymaps for switching panes

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -2,6 +2,12 @@ vim.keymap.set("n", "<esc>", vim.cmd.nohl, {})
 vim.keymap.set("n", "<leader>uu", function() vim.cmd.Lazy("check") end, {})
 vim.keymap.set("n", "<leader>x", vim.cmd.bd, {})
 vim.keymap.set("n", "<leader>X", function() vim.cmd.bufdo("bd") end, {})
+
+vim.keymap.set("n", "<c-h>", "<c-w>h", {})
+vim.keymap.set("n", "<c-j>", "<c-w>j", {})
+vim.keymap.set("n", "<c-k>", "<c-w>k", {})
+vim.keymap.set("n", "<c-l>", "<c-w>l", {})
+
 vim.keymap.set("v", "<leader>d", "y'>p", {})
 vim.keymap.set("v", "<", "<gv", {})
 vim.keymap.set("v", ">", ">gv", {})
