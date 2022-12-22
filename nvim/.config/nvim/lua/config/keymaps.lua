dd04a0ff24fc2154671c29441d28dcf22b861f71
nitevatt <nitevatt@outlook.com>
2022-12-22 12:02:16 +0100

    fix: change update handler

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,5 +1,5 @@
 vim.keymap.set("n", "<esc>", vim.cmd.nohl, {})
-vim.keymap.set("n", "<leader>uu", vim.cmd.PackerSync, {})
+vim.keymap.set("n", "<leader>uu", function() vim.cmd.Lazy("check") end, {})
 vim.keymap.set("n", "<leader>x", vim.cmd.bd, {})
 vim.keymap.set("n", "<leader>X", function() vim.cmd.bufdo("bd") end, {})
 vim.keymap.set("v", "<leader>d", "y'>p", {})
