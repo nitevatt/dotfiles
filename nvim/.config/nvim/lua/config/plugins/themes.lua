1e96a5afab8e5997085e209c4a1af9b07ce6b776
nitevatt <nitevatt@outlook.com>
2022-12-26 17:11:16 +0100

    feat: change theme to rose pine

--- nvim/.config/nvim/lua/config/plugins/themes.lua
+++ nvim/.config/nvim/lua/config/plugins/themes.lua
@@ -1,11 +1,14 @@
 local M = {
   -- "arcticicestudio/nord-vim",
-  "navarasu/onedark.nvim",
+  -- "navarasu/onedark.nvim",
+  "rose-pine/neovim",
+  name = "rose-pine"
 
 }
 
 M.config = function()
-  require("onedark").load()
+  -- require("onedark").load()
+  vim.cmd.colorscheme("rose-pine")
 end
 
 return M
