b3dc44e865d10714c1ddfe86b7bb73082497bbcf
nitevatt <nitevatt@outlook.com>
2022-12-28 21:45:10 +0100

    feat: try catppuccin theme

--- nvim/.config/nvim/lua/config/plugins/themes.lua
+++ nvim/.config/nvim/lua/config/plugins/themes.lua
@@ -1,14 +1,15 @@
 local M = {
   -- "arcticicestudio/nord-vim",
   -- "navarasu/onedark.nvim",
-  "rose-pine/neovim",
-  name = "rose-pine"
-
+  -- "rose-pine/neovim", name = "rose-pine",
+  "catppuccin/nvim", as = "catppuccin"
 }
 
 M.config = function()
+  -- vim.cmd.colorscheme("nord")
   -- require("onedark").load()
-  vim.cmd.colorscheme("rose-pine")
+  -- vim.cmd.colorscheme("rose-pine")
+  vim.cmd.colorscheme("catppuccin")
 end
 
 return M
