358020de7ae48e994dc67add706e93524ade9d01
nitevatt <nitevatt@outlook.com>
2024-10-16 15:06:37 +0200

    feat(nvim): refactor autocmd

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,7 +1,1 @@
-  vim.api.nvim_create_autocmd("CursorHold", {
-    group = vim.api.nvim_create_augroup("MyCursorHoldGroup", { clear = true }),
-    pattern = "*",
-    callback = function()
-      vim.cmd("silent! mode")
-    end
-  })
+vim.cmd("au CursorHold * silent! mode")
