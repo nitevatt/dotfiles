8cd167a6730b7be8b69dda17702511fa50802433
nitevatt <nitevatt@outlook.com>
2024-10-17 13:26:05 +0200

    feat(nvim/vscode): add keymaps

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,1 +1,18 @@
-vim.cmd("au CursorHold * silent! mode")
+vim.api.nvim_create_autocmd("CursorHold", {
+	group = vim.api.nvim_create_augroup("MyCursorHoldGroup", { clear = true }),
+	pattern = "*",
+	callback = function()
+		vim.cmd("mode")
+	end,
+})
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.keymap.set("n", "<tab>", function()
+	require("vscode").call("workbench.action.nextEditor")
+end)
+
+vim.keymap.set("n", "<s-tab>", function()
+	require("vscode").call("workbench.action.previousEditor")
+end)
