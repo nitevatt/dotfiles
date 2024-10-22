85cd22151ffcce32eb4abc0b528889003a9afaf4
nitevatt <nitevatt@outlook.com>
2024-10-22 22:10:04 +0200

    feat(wezterm): add config for wezterm

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,19 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config.color_scheme = "OneDark (base16)"
+config.initial_cols = 160
+config.initial_rows = 40
+config.font_size = 16.0
+config.use_fancy_tab_bar = false
+
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+
+return config
+
