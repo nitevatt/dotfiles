27c2061b1edd3d881d2d057ef8057da3679e6dad
nitevatt <nitevatt@outlook.com>
2024-10-23 16:57:13 +0200

    feat(wezterm): divide configs into macos and windows

--- wezterm/windows/.wezterm.lua
+++ wezterm/windows/.wezterm.lua
@@ -5,8 +5,9 @@ local config = wezterm.config_builder()
 config.color_scheme = "OneDark (base16)"
 config.initial_cols = 160
 config.initial_rows = 40
-config.font_size = 16.0
+config.font_size = 12.0
 config.use_fancy_tab_bar = false
+config.window_decorations = "RESIZE"
 
 config.window_padding = {
 	left = 0,
@@ -15,5 +16,6 @@ config.window_padding = {
 	bottom = 0,
 }
 
-return config
+config.default_domain = "WSL:Ubuntu-22.04"
 
+return config
