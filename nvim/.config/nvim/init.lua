c64e36e3607ca20b0ccc5c065a093856bfb1da2a
nitevatt <nitevatt@outlook.com>
2024-11-15 06:56:39 +0100

    feat(nvim/vscode): add mappings and configure updatetime

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -9,10 +9,22 @@ vim.api.nvim_create_autocmd("CursorHold", {
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+vim.o.updatetime = 300
+
+vim.keymap.set("n", "<esc>", vim.cmd.noh)
+
 vim.keymap.set("n", "<tab>", function()
 	require("vscode").call("workbench.action.nextEditor")
 end)
 
 vim.keymap.set("n", "<s-tab>", function()
 	require("vscode").call("workbench.action.previousEditor")
+end)
+
+vim.keymap.set("n", "<leader>fm", function()
+	require("vscode").call("editor.action.formatDocument")
+end)
+
+vim.keymap.set("n", "<leader>rn", function()
+	require("vscode").call("editor.action.rename")
 end)
