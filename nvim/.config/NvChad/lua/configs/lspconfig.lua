6d52452f0d8be95cbfaac7eff5ac3be70f215d1e
nitevatt <nitevatt@outlook.com>
2024-11-17 21:44:58 +0100

    fix(nvim/NvChad): disable file watch feature

--- nvim/.config/NvChad/lua/configs/lspconfig.lua
+++ nvim/.config/NvChad/lua/configs/lspconfig.lua
@@ -22,12 +22,16 @@ local servers = {
 
 local nvlsp = require "nvchad.configs.lspconfig"
 
+local capabilities = nvlsp.capabilities
+
+capabilities.workspace.didChangeWatchedFiles.dynamicRegistration = false
+
 -- lsps with default config
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     on_attach = nvlsp.on_attach,
     on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
+    capabilities = capabilities,
   }
 end
 
