6c455c691cbf4e091e4cc926c706b304f89f6e59
nitevatt <nitevatt@outlook.com>
2024-12-30 15:28:58 +0100

    feat(nvim): auto show complete item documentation

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -143,6 +143,12 @@ require("lazy").setup {
       },
       version = "*",
       opts = {
+        completion = {
+          documentation = {
+            auto_show = true,
+            auto_show_delay_ms = 200,
+          },
+        },
         keymap = {
           preset = "enter",
         },
