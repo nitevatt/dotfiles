3f93e51f1d8238b6e108f05d750674bafa38a26f
nitevatt <nitevatt@outlook.com>
2025-01-29 14:18:34 +0100

    feat(nvim): switch to catppuccin theme

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -68,16 +68,11 @@ vim.keymap.set("v", "<leader>d", "yPgv")
 require("lazy").setup {
   spec = {
     {
-      "rose-pine/neovim",
-      name = "rose-pine",
+      "catppuccin/nvim",
+      name = "catppuccin",
+      priority = 1000,
       config = function()
-        require("rose-pine").setup {
-          styles = {
-            italic = false,
-          },
-        }
-
-        vim.cmd.colorscheme "rose-pine"
+        vim.cmd.colorscheme "catppuccin"
 
         local function set_colorscheme()
           if vim.o.background == "dark" then
